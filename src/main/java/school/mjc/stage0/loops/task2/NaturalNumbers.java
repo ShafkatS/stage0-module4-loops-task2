459efb1c697722b8bbcbdc775c2431e6f3dda8bd
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 15:30:41 +0500

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int numbersBeforeLastPrinted = 0;
+        while (lastPrinted >= numbersBeforeLastPrinted) {
+            System.out.println(numbersBeforeLastPrinted);
+            numbersBeforeLastPrinted++;
+        }
     }
 }
