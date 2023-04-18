11ad57945202e61dae82afd72cb6360cd4b19d43
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 15:52:15 +0500

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,11 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int multiplicationByMultiplyByAndToInclusive;
-        System.out.println(multiplicationByMultiplyByAndToInclusive);
-            while (multiplyByAndToInclusive > multiplicationByMultiplyByAndToInclusive) {
-                multiplicationByMultiplyByAndToInclusive++;
-                System.out.println(multiplyByAndToInclusive * multiplicationByMultiplyByAndToInclusive);
+        if (multiplyByAndToInclusive>0){
+            int temp=0;
+            while(temp<=multiplyByAndToInclusive){
+                System.out.println(temp*multiplyByAndToInclusive);
+                temp++;
             }
+        } else if (multiplyByAndToInclusive<0) {
+            int temp=0;
+            int res = -1*multiplyByAndToInclusive;
+            while(temp<=res){
+                System.out.println(temp*multiplyByAndToInclusive);
+                temp++;
+            }
+        } 
     }
 }
