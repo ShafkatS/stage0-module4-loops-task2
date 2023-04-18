a15648f4e7001717ffcfbc38cd5b806aac05164e
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 15:32:01 +0500

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int numbersBeforeLastPrinted = 0;
+        while (lastPrinted >= numbersBeforeLastPrinted) {
+            System.out.println(numbersBeforeLastPrinted);
+            numbersBeforeLastPrinted++;
+        }
     }
 }
